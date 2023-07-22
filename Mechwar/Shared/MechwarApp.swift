6a0aba6cc5c0648a4ab5473c3a191c2639c7201b
charbel <charbelchedid06@outlook.com>
2023-07-22 04:09:39 +0300

    sdfljn

---
+++ Mechwar/Shared/MechwarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MechwarApp.swift
+//  Shared
+//
+//  Created by cbc04 on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MechwarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
